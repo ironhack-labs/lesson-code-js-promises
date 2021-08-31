2c34aab05945a60def8cefcd246abb35f37e3c4b
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-31 13:18:42 +0200

    Example - Promise.all() immediately resolved

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 "use strict";
 
-const p1 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve("foo"), 1000);
-});
+const p4 = Promise.all([]);
 
-const p2 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve(1337), 2000);
-});
-
-const p3 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve( { name: "Bob" } ), 4000);
-});
-
-
-Promise.all( [p1, p2, p3] )
-  .then((values) => console.log("values", values));
+console.log(p4);
