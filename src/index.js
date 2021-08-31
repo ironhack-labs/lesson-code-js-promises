882725ed7c9db4a31531346ef38349fac99a3882
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-31 12:19:29 +0200

    Example - Using promises instead of callbacks

--- src/index.js
+++ src/index.js
@@ -1,37 +1,27 @@
-"use strict";
-
 const directions = [
-  'Starting point: Ironhack Miami',
-  '↑ Head east on SW 8th St/Carlos Arboleya toward SW 1st Avenue',
-  '➔ Turn right onto S Miami Ave',
-  '* Chipotle Mexican Grill 891 S Miami Ave, Miami'
+  'Starting point: Ironhack Madrid',
+  '➔ Turn right toward P. de la Chopera',
+  '← At the roundabout, take the 1st exit onto P. de la Chopera',
+  '* Lune Creperie P. de la Chopera 33, Madrid'
 ];
 
 
-function getDirections(step, callback, errorCallback) {
-  setTimeout(() => {
-    console.log( directions[step] );
+function obtainDirections(step) {
+  return new Promise (function (resolve, reject) {
+    setTimeout(() => {
+      console.log( directions[step] );
+
+      if (!directions[step]) reject('Instructions not found.')
+      else resolve();
+    }, 2000); 
     
-    if (!directions[step]) errorCallback('Instructions not found.');
-    else callback();
-  }, 2000); 
-  
+  })
 }
 
-// Single callback
-// getDirections(0, ()=> {});
 
-
-// Callbacks in sequence
-getDirections(0, () => {
-  getDirections(1, () => {
-    getDirections(2, () => {
-      getDirections(3, () => {
-        
-        console.log('You arrived at your destination!');
-        // getDirections(4, () => {}, (err) => console.log(err) ) ;
-
-  	  }, (err) => console.log(err));
-    }, (err) => console.log(err));
-  }, (err) => console.log(err));
-}, (err) => console.log(err));
+obtainDirections(0)
+  .then(() => obtainDirections(1) )
+  .then(() => obtainDirections(2) )
+  .then(() => obtainDirections(3) )
+  .then(() => console.log('You arrived at your destination!') )
+  .catch((err) => console.log(err));
