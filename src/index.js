ec365ec1562cb40f5e2bc4f333ebd06a21dc9573
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-31 12:24:23 +0200

    Example - Creating a promise

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
-const directions = [
-  'Starting point: Ironhack Madrid',
-  '➔ Turn right toward P. de la Chopera',
-  '← At the roundabout, take the 1st exit onto P. de la Chopera',
-  '* Lune Creperie P. de la Chopera 33, Madrid'
-];
+"use strict";
 
-
-function obtainDirections(step) {
-  return new Promise (function (resolve, reject) {
-    setTimeout(() => {
-      console.log( directions[step] );
-
-      if (!directions[step]) reject('Instructions not found.')
-      else resolve();
-    }, 2000); 
-    
-  })
-}
-
-
-obtainDirections(0)
-  .then(() => obtainDirections(1) )
-  .then(() => obtainDirections(2) )
-  .then(() => obtainDirections(3) )
-  .then(() => console.log('You arrived at your destination!') )
-  .catch((err) => console.log(err));
+const myPromise = new Promise(function (resolve, reject) {
+   if (true) {  /* condition */
+      resolve(/* value */);  // fulfilled successfully
+   }
+   else {
+      reject(/* reason */);  // error, rejected
+   }
+});
