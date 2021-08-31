8fad9fc802789d32b26c25071f9d7165effa5ca1
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-31 13:12:50 +0200

    Example - Using the finally() method

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 "use strict";
 
-const pr7 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve('A'), 2000);
+const pr8 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("A"), 2000);
 });
 
 
-pr7
-  .then((value1) => {
-    console.log("1. then()", value1);  
-    throw new Error("FIRST ERROR");
-  })
-  .catch((err) => {
-    console.error("1. catch()", err);
-    return "Hello from catch";
-  })
-  .then((value2) => {
-    console.log("2. then()", value2);
-    throw new Error("SECOND ERROR");
-  })
-  .catch((err) => {
-    console.error("2. catch()", err);
+pr8
+  .then((value1) => console.log("1. then()"))
+  .then((value2) => console.log("2. then()"))
+  .finally(() => {
+    console.log("finally()");
   });
