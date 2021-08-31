e69233000a4fe92adb7fbbe7a749927f72b38f7a
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-31 13:14:49 +0200

    Additional example - Using the finally() method

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 "use strict";
 
-const pr8 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve("A"), 2000);
+const pr9 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Ironhack"), 2000);
 });
 
 
-pr8
-  .then((value1) => console.log("1. then()"))
-  .then((value2) => console.log("2. then()"))
-  .finally(() => {
-    console.log("finally()");
-  });
+pr9
+  .then(() => console.log("1. then()") )
+  .then(() => Promise.reject("Oops!") )       // <= Reject a Promise
+  .then(() => console.log("3. then()") )       // <= This block is skipped
+  .catch((err) => {
+    console.log("catch()", err) 
+  })
+  .finally(() => console.log("finally()"));
