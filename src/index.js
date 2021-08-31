fbf0d141f08871cee44e093734d91a8c95603655
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-31 12:13:21 +0200

    Example - callback hell

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 "use strict";
 
-// The `callback` parameter represents a function
+const directions = [
+  'Starting point: Ironhack Miami',
+  '↑ Head east on SW 8th St/Carlos Arboleya toward SW 1st Avenue',
+  '➔ Turn right onto S Miami Ave',
+  '* Chipotle Mexican Grill 891 S Miami Ave, Miami'
+];
 
-function func1(callback) {
-  console.log("Hi");
 
-  callback();
+function getDirections(step, callback, errorCallback) {
+  setTimeout(() => {
+    console.log( directions[step] );
+    
+    if (!directions[step]) errorCallback('Instructions not found.');
+    else callback();
+  }, 2000); 
+  
 }
 
-function func2() {
-  console.log("Goodbye!");
-}
+// Single callback
+// getDirections(0, ()=> {});
 
-func1( func2 );
+
+// Callbacks in sequence
+getDirections(0, () => {
+  getDirections(1, () => {
+    getDirections(2, () => {
+      getDirections(3, () => {
+        
+        console.log('You arrived at your destination!');
+        // getDirections(4, () => {}, (err) => console.log(err) ) ;
+
+  	  }, (err) => console.log(err));
+    }, (err) => console.log(err));
+  }, (err) => console.log(err));
+}, (err) => console.log(err));
