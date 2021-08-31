1fc99b6b08288e1152a70414bf2afdffcc3b66ec
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-31 12:14:42 +0200

    Save previous example

---
+++ src/previous-examples/03-callback-hell.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const directions = [
+  'Starting point: Ironhack Miami',
+  '↑ Head east on SW 8th St/Carlos Arboleya toward SW 1st Avenue',
+  '➔ Turn right onto S Miami Ave',
+  '* Chipotle Mexican Grill 891 S Miami Ave, Miami'
+];
+
+
+function getDirections(step, callback, errorCallback) {
+  setTimeout(() => {
+    console.log( directions[step] );
+    
+    if (!directions[step]) errorCallback('Instructions not found.');
+    else callback();
+  }, 2000); 
+  
+}
+
+// Single callback
+// getDirections(0, ()=> {});
+
+
+// Callbacks in sequence
+getDirections(0, () => {
+  getDirections(1, () => {
+    getDirections(2, () => {
+      getDirections(3, () => {
+        
+        console.log('You arrived at your destination!');
+        // getDirections(4, () => {}, (err) => console.log(err) ) ;
+
+  	  }, (err) => console.log(err));
+    }, (err) => console.log(err));
+  }, (err) => console.log(err));
+}, (err) => console.log(err));
