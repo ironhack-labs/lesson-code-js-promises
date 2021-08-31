e73697bb8c2fb1f567e6ef1fcb7838f0ba4eb613
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-31 12:58:49 +0200

    Update example - Chaining mulitple then blocks

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 "use strict";
 
-const pr5 = new Promise((resolve, reject) => {
+const pr6 = new Promise((resolve, reject) => {
   setTimeout(() => resolve("A"), 2000);
 });
 
 
-pr5
+pr6
   .then((value1) => {
     console.log("value1:", value1);
-    return "B";
+    return new Promise((resolve, reject) => setTimeout(() => resolve("B"), 2000));
   })
   .then((value2) => {
     console.log("value2:", value2);
-    return "C";
+    return new Promise((resolve, reject) => setTimeout(() => resolve("C"), 2000));
   })
   .then((value3) => {
     console.log("value3:", value3);
-    return "D";
+    return new Promise((resolve, reject) => setTimeout(() => resolve("D"), 2000));
   })
   .then((value4) => {
     console.log("value4:", value4);
